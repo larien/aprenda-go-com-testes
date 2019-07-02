807daa4dc866d6546d5402c52353f51af01285e3
Lauren Ferreira <lauren.ferremch@gmail.com>
2019-07-02 20:57:50 -0300

    Remove latex engine

--- build.books.sh
+++ build.books.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker run -v `pwd`:/source jagregory/pandoc -f markdown -o aprenda-go-com-testes.pdf --variable urlcolor=blue --toc --toc-depth=1 pdf-cover.md \
+docker run -v `pwd`:/source jagregory/pandoc -o learn-go-with-tests.pdf --variable urlcolor=blue --toc --toc-depth=1 pdf-cover.md \
     gb-readme.md \
     why.md \
     hello-world.md \
@@ -28,7 +28,7 @@ docker run -v `pwd`:/source jagregory/pandoc -f markdown -o aprenda-go-com-teste
     os-exec.md \
     error-types.md \
 
-docker run -v `pwd`:/source jagregory/pandoc -f markdown -o aprenda-go-com-testes.epub --toc --toc-depth=1 title.txt \
+docker run -v `pwd`:/source jagregory/pandoc -o learn-go-with-tests.epub --toc --toc-depth=1 title.txt \
     gb-readme.md \
     why.md \
     hello-world.md \
