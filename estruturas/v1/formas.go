e4bb5c7eef3d655160bfefb27d216d7a67ac8bec
Lauren Maria Ferreira <lauren.ferremch@gmail.com>
2019-12-24 22:15:34 -0300

    Tradução dos códigos

--- estruturas/v1/formas.go
+++ estruturas/v1/formas.go
@@ -1,6 +1,6 @@
 package main
 
-// Perímetro retorna o perímetro de um retângulo
+// Perimetro retorna o perímetro de um retângulo
 func Perimetro(largura float64, altura float64) float64 {
 	return 2 * (largura + altura)
 }
