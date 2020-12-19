4527b46f9e68cf4635324ce60f3eacf557d03b23
Lauren Ferreira <lauren.ferremch@gmail.com>
2020-12-19 13:35:41 -0300

    Update integracao_servidor_test.go

--- criando-uma-aplicacao/io/v1/integracao_servidor_test.go
+++ criando-uma-aplicacao/io/v1/integracao_servidor_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+func TestArmazenandoERetornandoVitorias TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	armazenamento := NovoArmazenamentoDeJogadorNaMemoria()
 	servidor := NovoServidorDoJogador(armazenamento)
 	jogador := "Pepper"
